f8671e091d1a3a5b5a84c9733c8b097bea8fbd55
Prem Kumar <prem68265@gmail.com>
2024-05-13 07:32:13 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import './App.css'
 import { useState } from 'react';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
-
 
 function App() {
 
